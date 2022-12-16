a2e0e5ddfb58fa314bdc6323ccdcad29d855104b
w7089 <3884662+w7089@users.noreply.github.com>
2022-12-16 15:17:17 +0200

    Create Jenkinsfile-Podman.groovy

---
+++ Jenkinsfile-Podman.groovy
@@ -0,0 +1,15 @@
+pipeline {
+    agent  {
+        label 'podman'
+    }
+
+    stages {
+        stage("build image") {
+            steps {
+                script {
+                    sh "podman build jenkins -t docker-in-jenkins"
+                }
+            }
+        }
+    }
+}
